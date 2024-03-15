7eed875b1bffc5d49049deaa2aa2de26a204142c
Fanaina Alves <fanaiina.allves@gmail.com>
2024-03-15 11:45:28 -0300

    Implemented class ServiceRepository.

--- src/main/java/com/ms/service/repository/ServiceRepository.java
+++ src/main/java/com/ms/service/repository/ServiceRepository.java
@@ -1,4 +1,10 @@
 package com.ms.service.repository;
 
-public interface ServiceRepository {
+import com.ms.service.model.ServiceModel;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface ServiceRepository extends MongoRepository<ServiceModel, String> {
+    Optional<ServiceModel> findByName(String name);
 }
